5e73681cc138e00e3c3669d0bf54b4858a36d541
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2025-03-23 19:21:58 +0100

    fixed light mode issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout({
         <ThemeProvider
             attribute="class"
             defaultTheme="light"
-            enableSystem
             disableTransitionOnChange
           >
             {children}
